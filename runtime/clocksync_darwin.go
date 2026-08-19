75a805ae229be302f598d0e7a79dc9abc517df98
Chris Ricketts <chris_ricketts@protonmail.com>
2026-08-19 21:10:22 +0100

    fix(lint): squash unused verifyClockSourceEnabled function on darwin

--- runtime/clocksync_darwin.go
+++ runtime/clocksync_darwin.go
@@ -3,5 +3,7 @@ package runtime
 import "context"
 
 func StartClockSyncer(ctx context.Context) (context.Context, error) {
+	// squash unused lint on darwin
+	_ = verifyClockSourceEnabled()
 	return ctx, nil
 }
